8fddbd83bd3f1c44a55d599fab00da79b8b37ffd
Ruben Espinosa <rderoldan1@gmail.com>
2016-05-10 21:54:09 -0500

    Mailer tests

--- test/mailers/people_mailer_test.rb
+++ test/mailers/people_mailer_test.rb
@@ -1,20 +1,25 @@
 require 'test_helper'
 
 class PeopleMailerTest < ActionMailer::TestCase
+  setup do
+    @from = people(:one)
+    @to = people(:two)
+  end
+
   test "created" do
-    mail = PeopleMailer.created
+    mail = PeopleMailer.created(@from.full_name, @to.id)
     assert_equal "Created", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    assert_equal [@to.email], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "has joined the application", mail.body.encoded
   end
 
   test "deleted" do
-    mail = PeopleMailer.deleted
+    mail = PeopleMailer.deleted(@from.full_name, @from.id)
     assert_equal "Deleted", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    assert_equal [@from.email], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "has left the application.", mail.body.encoded
   end
 
 end
